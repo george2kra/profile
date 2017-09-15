cc4cd51d2ec4a5639d077180fa83392e7a3de4ea
george2kra <g2kralevski@gmail.com>
2017-09-15 11:26:22 +1000

    initial add for profile page

---
+++ content.js
@@ -0,0 +1,13 @@
+var twitter = " <li> <a href='https://twitter.com/George2kr' target='_blank'>Twitter</a> </li> "
+var LinkedIn = " <li> <a href='https://www.linkedin.com/in/george-kralevski-8094962/' target='_blank'>LinkedIn</a> </li> "
+var GitHub = " <li> <a href='https://github.com/george2kra' target='_blank'>Github</a> </li> "
+var Contect_Me = " <li> <a href='contactme.html'> Contact Me </li> "
+var About_Me = " <li> <a href='aboutme.html'> About Me </a> </li> "
+
+var conCatenated =  LinkedIn +
+                    twitter +
+                    GitHub +
+                    Contect_Me +
+                    About_Me
+
+document.write(conCatenated);
