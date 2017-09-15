a71682d4f2c5f107f4e7cbfb26f429ed636a4e47
george2kra <g2kralevski@gmail.com>
2017-09-15 13:37:55 +1000

    added warehouse inventory projects html

---
+++ projlist.js
@@ -0,0 +1,9 @@
+var warehouseinventory = " <li> <a href='projinventory.html'> Warehouse Inventory </a> </li> "
+var framedtext = " <li> <a href='projframedtext.html'> Text in a fame </a> </li> "
+var rainfallcollection = " <li> <a href=''> Rain Fall Collection </a> </li>"
+
+var conCatenated =  warehouseinventory +
+                    framedtext +
+                    rainfallcollection
+
+document.write(conCatenated);
