6495e80250858a0d3dbbfffa11c405dd63f041d7
george2kra <g2kralevski@gmail.com>
2017-09-17 15:39:44 +1000

    changed wording in profile page

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var twitter = " <li> <a href='https://twitter.com/George2kr' target='_blank'>Twitter</a> </li> "
 var LinkedIn = " <li> <a href='https://www.linkedin.com/in/george-kralevski-8094962/' target='_blank'>LinkedIn</a> </li> "
 var GitHub = " <li> <a href='https://github.com/george2kra' target='_blank'>Github</a> </li> "
-var Contect_Me = " <li> <a href='contactme.html'> Contact Me </li> "
+var Contect_Me = " <li> <a href='contactme.html'> Contact Me</a> </li> "
 var About_Me = " <li> <a href='aboutme.html'> About Me </a> </li> "
 
 var conCatenated =  LinkedIn +
